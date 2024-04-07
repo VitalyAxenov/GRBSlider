a0a0932b1d862d6b389ed651c3c3a56f77684421
Vitaly Axenov <hrmobs@gmail.com>
2024-04-07 18:10:22 +0300

    Initial Commit

---
+++ GRBSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GRBSlider
+//
+//  Created by Виталий Аксенов on 07.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
